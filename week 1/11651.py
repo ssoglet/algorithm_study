574538e449a2386213f4fcce909d70e5c916885d
ssoglet <128627574+ssoglet@users.noreply.github.com>
2023-03-25 02:33:27 +0900

    0325:[11651,O]

---
+++ week 1/11651.py
@@ -0,0 +1,11 @@
+n = int(input())
+num_list = [[0 for i in range(2)] for j in range(n)]
+
+for i in range(n):
+    x, y = input().split()
+    num_list[i][0] = int(x)
+    num_list[i][1] = int(y)
+
+num_list.sort(key=lambda x: (x[1], x[0]))
+for x, y in num_list:
+    print(x, y)
